627fb8d22c491b77163c2df4b667095d5f9f20f9
山村勇貴 <yamamurayuuki@yamamurayuuki-no-MacBook-Pro.local>
2021-03-07 20:21:22 +0900

    useState

--- reactApp/my-app/src/App.js
+++ reactApp/my-app/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const App = () => {
+  const [count, setCount] = useState(0);
 
-function App() {
+  const handliPulsClick = () => {
+    setCount(count + 1);
+  }
+
+  const handliMinusClick = () => {
+    setCount(count - 1);
+  }
+
+  const handliPulsClick2  = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  }
+  const handliMinusClick2  = () => {
+    setCount((prevCount) => {
+      return prevCount - 1;
+    });
+  }
+
+  const resetClick = () => {
+    setCount((prevCount) => {
+      prevCount = 0;
+      return prevCount;
+    });
+  }
+
+  const twoClick = () => {
+    setCount(count * 2)
+  }
+
+  const division = () => {
+    setCount((prevCount) => {
+      if(prevCount % 3 === 0){
+        return prevCount / 3;
+      }else{
+        return prevCount;
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        Count:{count}
+      </div>
+      <div>
+        <button onClick={handliPulsClick}>+1</button>
+        <button onClick={handliMinusClick}>-1</button>
+      </div>
+      <div>
+        <button onClick={handliPulsClick2}>+1</button>
+        <button onClick={handliMinusClick2}>-1</button>
+      </div>
+      <div>
+        <button onClick={resetClick}>Reset</button>
+        <button onClick={twoClick}>*2</button>
+        <button onClick={division}>3の倍数の時３で割る</button>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App
